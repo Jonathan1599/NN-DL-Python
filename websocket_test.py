7a347cf4fd3e8a9d6d1228eb1e68ca769b434e34
Jonathan D'mello <jonathandmello@yahoo.co.in>
2021-11-30 18:46:41 +0530

    websocket start / not working

--- websocket_test.py
+++ websocket_test.py
@@ -1,29 +1,16 @@
-
 import websockets
 import asyncio
 
-from flask import Flask
+PORT = 7890
 
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-   app.run(host="localhost", port=3002, debug=True)
-
-
-   PORT = 3000
+print("Server listening on Port " + str(PORT))
 
 async def echo(websocket, path):
     print("A client just connected")
     try:
         async for message in websocket:
             print("Received message from client: " + message)
+            await websocket.send("Pong: " + message)
     except websockets.exceptions.ConnectionClosed as e:
         print("A client just disconnected")
 
